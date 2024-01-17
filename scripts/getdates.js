5b02ce98d43584bc550a580830ff64f618505b4e
gianvin <ginamangahassanchezong@gmail.com>
2024-01-17 21:08:40 +0800

    edited the base.css and getdates.csjs

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,12 @@
-alert(document.lastModified);
+document.addEventListener("DOMContentLoaded", function () {
+    var lastModifiedDate = new Date();
+
+    var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', seconds: 'numeric' };
+
+    var formattedDate = lastModifiedDate.toLocaleDateString('en-US', options);
+
+    document.getElementById("lastModified").textContent = "Last Modification: " + formattedDate;
+})
+
+
 
