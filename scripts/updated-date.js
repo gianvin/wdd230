12993147a29cd8a2b1eb29d529b3b7e560189f52
gianvin <ginamangahassanchezong@gmail.com>
2024-01-27 13:05:12 +0800

    edited name of white space file

--- scripts/updated-date.js
+++ scripts/updated-date.js
@@ -1,21 +1,9 @@
-//document.addEventListener("DOMContentLoaded", function () {
-//var lastModifiedDate = new Date();
+document.addEventListener("DOMContentLoaded", function () {
+    var lastModifiedDate = new Date();
 
-//var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', seconds: 'numeric' };
+    var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', seconds: 'numeric' };
+    var formattedDate = lastModifiedDate.toLocaleDateString('en-US', options);
 
-// var formattedDate = lastModifiedDate.toLocaleDateString('en-US', options);
-
-// document.getElementById("lastModified").textContent = "Last Updated: " + formattedDate;
-//})//
-
-function getCurrentDate() {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-}
-
-document.getElementById('lastUpdated').textContent = getCurrentDate();
-
+    document.getElementById("lastUpdated").textContent = "Last Updated: " + formattedDate;
+})
 
