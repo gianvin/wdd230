1afb7bbef97bfb49ff064a92bb9a16a723f25975
gianvin <ginamangahassanchezong@gmail.com>
2024-01-27 22:50:59 +0800

    copy scripts in lesson 3

---
+++ lesson03/scripts/getdates.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var lastModifiedDate = new Date();
+
+    var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', seconds: 'numeric' };
+
+    var formattedDate = lastModifiedDate.toLocaleDateString('en-US', options);
+
+    document.getElementById("lastModified").textContent = "Last Modification: " + formattedDate;
+});
+
+
+// script for hamburger menu//
+const hamButton = document.querySelector('#menu');
+const nav = document.querySelector('.nav');
+const closeButton = document.querySelector('#closeButton');
+
+hamButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+closeButton.addEventListener('click', () => {
+    nav.classList.remove('open');
+    hamButton.classList.remove('open');
+});
