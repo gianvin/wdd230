7761d7c877db5d50e52d18db9cd13f265438a12f
gianvin <ginamangahassanchezong@gmail.com>
2024-03-05 20:33:22 +0800

    edited the script

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -15,19 +15,20 @@ const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         //elements to add in the div.cards element in html
         let card = document.createElement('section');
-        let fullName = document.createElement('___');
+        let fullName = document.createElement('Wilford Woodruff');
         let portrait = document.createElement('img');
 
         //h2 content to show the prophet's full name
-        fullName.textContent = `${prophet.______} _____________`;
+        fullName.textContent = `${prophet.wilfordWoodruff} Wilford Woodruff`;
         // build image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
-        porttrait.setAttribute('alt', `Portrait of ${prophet._____} _____________`);
+        porttrait.setAttribute('alt', `Portrait of ${prophet.wilfordWoodruff_} Wilford Woodruff`);
+        portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
         //Append the section (crad) with the created element
-        card.appendChild(________);
+        card.appendChild(Wilford);
         card.appendChild(portrait);
 
         cards.appendChild(card);
