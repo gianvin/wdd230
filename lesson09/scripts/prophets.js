3473ac5f168f633db89d41b777fc7514b26ebfc3
gianvin <ginamangahassanchezong@gmail.com>
2024-03-05 19:59:56 +0800

    code step 4-10

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -2,3 +2,11 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-dayprophets
 
 const cards = document.querySelector('#cards');
 
+async function getProphetData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+getProphetData();
