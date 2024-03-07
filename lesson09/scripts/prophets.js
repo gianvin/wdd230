98a402fc7e673edbb60a45c8d10414e13d232a92
gianvin <ginamangahassanchezong@gmail.com>
2024-03-07 20:36:36 +0800

    edited the css

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,7 +1,38 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-dayprophets.json';
 
-const cards = document.querySelector('#cards');
+const cards = document.querySelector("#cards");
 
+
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        //elements to add in the div.cards element in html
+        let card = document.createElement("section");
+        let fullName = document.createElement("h2");
+        let portrait = document.createElement("img");
+        let birthDate = document.createElement("p");
+        let birthPlace = document.createElement("p");
+
+        //h2 content to show the prophet's full name, birthdate and birthplace
+        fullName.textContent = `${prophet.name} ${prophet, lastname}`;
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Pace of Birth: ${prophet.birthplace}`;
+        // build image portrait by setting all the relevant attributes
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("height", "440");
+
+        //Append the section (crad) with the created element
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    })
+}
 async function getProphetData(url) {
     const response = await fetch(url);
     const data = await response.json();
@@ -10,27 +41,3 @@ async function getProphetData(url) {
 }
 
 getProphetData();
-
-const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
-        //elements to add in the div.cards element in html
-        let card = document.createElement('section');
-        let fullName = document.createElement('Wilford Woodruff');
-        let portrait = document.createElement('img');
-
-        //h2 content to show the prophet's full name
-        fullName.textContent = `${prophet.wilfordWoodruff} Wilford Woodruff`;
-        // build image portrait by setting all the relevant attributes
-        portrait.setAttribute('src', prophet.imageurl);
-        porttrait.setAttribute('alt', `Portrait of ${prophet.wilfordWoodruff_} Wilford Woodruff`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
-
-        //Append the section (crad) with the created element
-        card.appendChild(wilfordWoodruff);
-        card.appendChild(portrait);
-
-        cards.appendChild(card);
-    });
-}
