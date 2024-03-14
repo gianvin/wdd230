7c81a928968a95fbc9b3818662c1907e6908f8a8
gianvin <ginamangahassanchezong@gmail.com>
2024-03-14 21:52:22 +0800

    edited the weather.js base on the errors in the dev tool console

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,6 +1,6 @@
 const currentTemp = document.querySelector("#current-temp");
-const weatherIcon = document.queryselector("#weather-icon");
-const captionDesc = document.queryselector("figcaption");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=927c80f45d4cb1121616e5d1ffc5c706";
 
@@ -11,28 +11,33 @@ async function fetchData() {
 }
 
 async function updatePage() {
-    const data = await fetchData();
-    console.log(data);
+    try {
+        const data = await fetchData();
+        console.log(data);
 
-    const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon.png}`;
+        if (!data || !data.weather || data.weather.length === 0) {
+            throw new Error("Weather data not available");
+        }
 
-    const temp = `${data.main.temp}`;
-    currentTemp.innerHTML = `${temp}&deg;F`;
+        const temp = Math.round(data.main.temp);
+        currentTemp.innerHTML = `${temp}&deg;F`;
 
-    let desc = `${data.weather[0].description}`;
-    let myDesc = capitalizedFirstLetters(desc);
-    weatherIcon.setAttribute("src", iconSrc);
-    weatherIcon.setAttribute("alt", desc);
-    captionDesc.textContent = `${myDesc}`;
-}
+        let desc = "";
+        for (let i = 0; i < data.weather.length; i++) {
+            desc += `${data.weather[i].description.charAt(0).toUpperCase()}${data.weather[i].description.slice(1)}`;
+            if (i !== data.weather.length - 1) {
+                desc += ", ";
+            }
+        }
 
-function capitalizedFirstLetters(words) {
-    const mySentence = words.split(" ");
-    for (let i = 0; 1 < mySentence.length; i++) {
-        mySentence[i] = mysentence[i][0].toUpperCase() + mySentence[i].substr(1);
+        weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+        weatherIcon.setAttribute("alt", desc);
+        captionDesc.textContent = desc;
+    } catch (error) {
+        console.error("An error occured:", error);
     }
-    return mySentence.join(" ");
 }
+
 
 updatePage();
 
