23f18b56583a78fe472df80a460c595a06fa6a22
gianvin <ginamangahassanchezong@gmail.com>
2024-03-15 12:58:54 +0800

    edited the css to adjust the style of Information

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -41,4 +41,44 @@ pageVisits++;
 localStorage.setItem("pageVisits-localStorage", pageVisits);
 
 //script for Weather API//
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
 
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=14.77&lon=121.02&units=imperial&appid=927c80f45d4cb1121616e5d1ffc5c706";
+async function fetchData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+async function updatePage() {
+    try {
+        const data = await fetchData();
+        console.log(data);
+
+        if (!data || !data.weather || data.weather.length === 0) {
+            throw new Error("Weather data not available");
+        }
+
+        const temp = Math.round(data.main.temp);
+        currentTemp.innerHTML = `${temp}&deg;F`;
+
+        let desc = "";
+        for (let i = 0; i < data.weather.length; i++) {
+            desc += `${data.weather[i].description.charAt(0).toUpperCase()}${data.weather[i].description.slice(1)}`;
+            if (i !== data.weather.length - 1) {
+                desc += ", ";
+            }
+        }
+
+        weatherIcon.setAttribute("src", `https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+        weatherIcon.setAttribute("alt", desc);
+        captionDesc.textContent = desc;
+    } catch (error) {
+        console.error("An error occured:", error);
+    }
+}
+
+
+updatePage();
